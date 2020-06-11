3140fd3400a5522955e5e0699f37aec083962d4e
killinux <killinux@163.com>
2020-06-11 15:14:18 +0800

    the example about two wasm interaction

--- chapter-07-cook-the-books/testc/add.c
+++ chapter-07-cook-the-books/testc/add.c
@@ -3,3 +3,6 @@
 int add(int a,int b){
     return a+b;
 }
+int addtest(){
+    return 2*2;
+}
--- chapter-07-cook-the-books/testc/test.c
+++ chapter-07-cook-the-books/testc/test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int add(int a,int b);
 int thistest(){
     return 2*4;
 }
@@ -12,14 +12,17 @@ struct Node {
   struct Node *next;
   struct Node *prev;
 };
+int testadd(){
+    return add(2,5);
+}
 int testnode(){
     struct Node *newNode = (struct Node *) malloc(sizeof(struct Node));
     newNode->id = 124;
     printf("newnode before:%d \n",newNode->id);
     //free(newNode);//不能用free
-    printf("newnode after:%d \n",newNode->id);
+//    printf("newnode after:%d \n",newNode->id);
     return  newNode->id;
 }
-int main(){
-    testnode();
-}
+//int main(){
+//    testnode();
+//}
